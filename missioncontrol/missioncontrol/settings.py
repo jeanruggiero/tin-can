abd082d7eadf054a18c9cd1ef2a79faffe4d6f30
Jean Ruggiero <jeanruggiero@gmail.com>
2020-09-22 18:09:46 -0700

    Updated web app settings and added gunicorn to requirements

--- missioncontrol/missioncontrol/settings.py
+++ missioncontrol/missioncontrol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e+83lg2gy78fzvlg@g%&@j*uoyio)v_gno8xf(v==uxi8*-f80'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
